e4e39a697bae6c02af82de14a17658f7e8a6696c
IgorShcherbakov <igor.eng.prog@gmail.com>
2025-03-31 19:44:43 +0300

    Скорректировал Dockerfile. Исправил в скрипте kafka-init.sh тестовые топики на осмысленный. Исправил роут в сервисе shopapi. Подготовил файл products.json для будущей загрузки в него списка товаров.

--- scripts/kafka-init.sh
+++ scripts/kafka-init.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-# Команда для создания топика topic-1
-kafka-topics --create --topic topic-1 --bootstrap-server kafka-0:9010 --partitions 3 --replication-factor 2 --command-config /etc/kafka/secrets/adminclient-configs.conf
+# Команда для создания топика products
+kafka-topics --create --topic products --bootstrap-server kafka-0:9010 --partitions 3 --replication-factor 2 --command-config /etc/kafka/secrets/adminclient-configs.conf
 
-# Команда для создания топика topic-2
-kafka-topics --create --topic topic-2 --bootstrap-server kafka-0:9010 --partitions 3 --replication-factor 2 --command-config /etc/kafka/secrets/adminclient-configs.conf
-
-# Команда для создания топика topic-3
-kafka-topics --create --topic topic-3 --bootstrap-server kafka-0:9010 --partitions 3 --replication-factor 2 --command-config /etc/kafka/secrets/adminclient-configs.conf
-
-# Дать продюсеру права на запись в топик topic-1
+# Дать продюсеру права на запись в топик products
 kafka-acls --bootstrap-server kafka-0:9010  \
 --command-config /etc/kafka/secrets/adminclient-configs.conf \
 --add   --allow-principal User:producer \
 --allow-principal User:producer  \
 --operation write \
---topic topic-1 
-
-# Дать продюсеру права на запись в топик topic-2
-kafka-acls --bootstrap-server kafka-0:9010  \
---command-config /etc/kafka/secrets/adminclient-configs.conf \
---add   --allow-principal User:producer \
---allow-principal User:producer  \
---operation write \
---topic topic-2 
+--topic products 
 
 # Дать консьюмеру права на чтение топика topic-1
 kafka-acls --bootstrap-server kafka-0:9010  \
