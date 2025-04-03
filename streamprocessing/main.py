4e33b6fee7d2422245f4f7b38732383fc662cc14
IgorShcherbakov <igor.eng.prog@gmail.com>
2025-04-04 01:21:39 +0300

    Рефакторинг сервиса streamprocessing.

--- streamprocessing/main.py
+++ streamprocessing/main.py
@@ -3,15 +3,9 @@ import faust
 import logging
 import ssl
 from confluent_kafka import serialization
-
-# Установите переменные окружения для SSL
-os.environ['KAFKA_SSL_CA_LOCATION'] = 'ca.crt'
-os.environ['KAFKA_SSL_CERTIFICATE_LOCATION'] = 'kafka-1.crt'
-os.environ['KAFKA_SSL_KEY_LOCATION'] = 'kafka-1.key'
-os.environ['KAFKA_SECURITY_PROTOCOL'] = 'SASL_SSL'
-os.environ['KAFKA_SASL_MECHANISM'] = 'PLAIN'
-os.environ['KAFKA_SASL_USERNAME'] = 'consumer'
-os.environ['KAFKA_SASL_PASSWORD'] = 'consumer'
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.json_schema import JSONDeserializer
+from confluent_kafka.serialization import SerializationContext, MessageField
 
 logging.basicConfig(
     level=logging.INFO, 
@@ -36,8 +30,6 @@ app = faust.App(
     broker_credentials=faust.SASLCredentials(
         username='admin',
         password='admin-secret',
-        #mechanism='PLAIN',
-        #security_protocol='SASL_SSL',
         ssl_context=ssl_context
     )
 )
@@ -49,10 +41,31 @@ ban_products_table = app.Table(
     default=dict,     # Функция или тип для пропущенных ключей
 )
 
+# Настройка клиента Schema Registry
+schema_registry_conf = {'url': 'http://schema-registry:8081'}
+schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+# Получение схемы из Schema Registry
+subject_name = "products-value"
+schema_response = schema_registry_client.get_latest_version(subject_name)
+json_schema_str = schema_response.schema.schema_str
+
+# Настройка десериализатора
+def from_dict(data, ctx):
+    return data
+
+json_deserializer = JSONDeserializer(
+    schema_str=json_schema_str, 
+    schema_registry_client=schema_registry_client, 
+    from_dict=from_dict
+)
 string_deserializer = serialization.StringDeserializer()
 
 # топик заблокированных товаров для входных данных
 ban_products_topic = app.topic("ban-products", key_type=str, value_type=str)
+
+# топик заблокированных товаров для входных данных
+products_topic = app.topic("products", key_type=str, value_type=bytes)
 
 # Функция, реализующая потоковую обработку заблокированных товаров       
 @app.agent(ban_products_topic)
@@ -72,6 +85,18 @@ async def process_ban_products(ban_products):
         logger.info(f"Новый список заблокированных товаров:")
         for key, value in ban_products_table.items():
             logger.info(f"Ключ: {key}, Значение: {value}")
+
+# Функция, реализующая потоковую обработку сообщений
+@app.agent(products_topic)
+async def process_messages(products):
+    async for product in products:#.filter(lambda msg: json.loads(msg).get('product_id') not in ban_products_table[json.loads(msg).get('product_id')]):
+        try:
+            logger.info(f"Получено product: {product}")
+            msg = json_deserializer(product, SerializationContext(products_topic.get_topic_name(), MessageField.VALUE))
+            logger.info(f"Получен товар: {msg.get('product_id')}")
+            yield msg
+        except Exception as e:
+            logger.exception(e)
 
 
 if __name__ == "__main__":
